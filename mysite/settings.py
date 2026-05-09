deb7371584e1c9af1e47be068df1e5ebe823af42
Mr.srp <mahdighan1234@gmail.com>
2026-05-09 07:57:29 -0700

    add static directories and urls in settings
    static root
    static url
    media url
    media  root

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,7 +86,8 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                 'UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
@@ -116,6 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'mysite',
+]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,18 @@ from django.contrib import admin
 
 from django.urls import path ,include
 
+from django.conf import settings
+
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.url')),
 ]
+
+urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # ... the rest of your URLconf goes here ...
+urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # ... the rest of your URLconf goes here ...
+
